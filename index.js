269bdc361d14c5bfb6d23854989bc335bfb69a0e
miriammo8467 <86227859+miriammo8467@users.noreply.github.com>
2025-03-25 12:09:06 +0200

    ה גדול

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ const options = {
 };
 
 // Endpoint שמחזיר את רשימת האפליקציות המותקנות
+app.get(()=>{
+    res.send('Hello World');
+});
 app.get('/api/render-services', (req, res) => {
     axios
         .request(options)
